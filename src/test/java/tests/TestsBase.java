5458ba62f79cb6cc641b7253fbaddba34edeb3d4
travi4ka <sergey.zhiharev@mbicycle.com>
2023-02-05 15:38:36 +0300

    remote + allure

--- src/test/java/tests/TestsBase.java
+++ src/test/java/tests/TestsBase.java
@@ -18,10 +18,10 @@ public class TestsBase {
     @BeforeAll
     public static void beforeAll() {
         Configuration.browser = System.getProperty("browser","chrome");
-      //  Configuration.browserVersion = "100.0";
+        Configuration.browserVersion = "100.0";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-      //  Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
