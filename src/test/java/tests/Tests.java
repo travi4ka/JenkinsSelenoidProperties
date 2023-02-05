46a3955732e37813725eb6facb7d989f427e38cf
travi4ka <sergey.zhiharev@mbicycle.com>
2023-02-05 14:21:02 +0300

    second

---
+++ src/test/java/tests/Tests.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static testData.TestData.*;
+@Tag("example")
+public class Tests extends TestsBase {
+    @Test
+    public void fillOutForm() {
+        automationPracticeFormPage
+                .openPage()
+                .setUserFirstNameInput(nameFirst)
+                .setUserLastNameInput(nameLast)
+                .setUserEmailInput(email)
+                .setUserPhoneInput(phone)
+                .setUserGenderSection(gender)
+                .setUserBOD(birthDateDay, birthDateMonth, birthDateYear)
+                .setSubjects(subject)
+                .setHobbies(hobby)
+                .setImage(pictureName)
+                .setCurrentAddressInput(currentAddress)
+                .setState(state)
+                .setCity(city)
+                .clickSubmitButton()
+                .verifyTableValues("Student Name", nameFirst + " " + nameLast)
+                .verifyTableValues("Student Email", email)
+                .verifyTableValues("Gender", gender)
+                .verifyTableValues("Mobile", phone)
+                .verifyTableValues("Date of Birth", birthDateDay + " " + birthDateMonth + "," + birthDateYear)
+                .verifyTableValues("Hobbies", hobby)
+                .verifyTableValues("Picture", pictureName)
+                .verifyTableValues("Address", currentAddress)
+                .verifyTableValues("State and City", state + " " + city)
+                .verifyTableValues("Subjects", subject);
+    }
+}
